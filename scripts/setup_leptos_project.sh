114e8005dc903cfc67452414018af6b857d3dcbf
DrFlowerkick <flowerkick14@gmail.com>
2025-07-28 19:00:33 +0200

    Fixing setup script to update app name in publish.yml.

--- scripts/setup_leptos_project.sh
+++ scripts/setup_leptos_project.sh
@@ -324,6 +324,8 @@ EXPOSE 8080
 CMD ["/app/$PROJECT_NAME"]
 EOF
 
+echo "17. Projektname im publish.yml Workflow anpassen"
+sed -i "s|SET_APP_NAME_HERE|$PROJECT_NAME|" .github/workflows/publish.yml
 
 echo "✅ Setup abgeschlossen für Projekt: $PROJECT_NAME"
 echo "➡️ Passe jetzt oder später das README.md für $PROJECT_NAME manuell an."
